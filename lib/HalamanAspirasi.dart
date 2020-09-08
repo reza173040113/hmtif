c3ae3a8b93f8c45af1b390286c4bc15e5c06757d
reza173040113 <reza_173040113@mail.unpas.ac.id>
2020-09-09 06:20:50 +0700

    halaman detail tinggal dimodif fungsi sudah berhasil

--- lib/HalamanAspirasi.dart
+++ lib/HalamanAspirasi.dart
@@ -1,9 +1,8 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'HalamanDetailAspirasi.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -22,6 +21,15 @@ Future<void> main() async {
 }
 
 class Aspirasi extends StatelessWidget {
+  navigateToDetail(BuildContext context,DocumentSnapshot post) async{
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DetailAspirasi(
+                  MyStudent: post,
+                )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,30 +42,35 @@ class Aspirasi extends StatelessWidget {
                 stream: Firestore.instance.collection("MyStudents").snapshots(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, index) {
-                        // Firebase.initializeApp();
-                        DocumentSnapshot documentSnapshot =
-                            snapshot.data.documents[index];
-                        return SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Card(
-                            child: ListTile(
-                              title: Text(documentSnapshot.data()['name']),
-                              subtitle:
-                                  Text(documentSnapshot.data()['studentId']),
-                              leading: CircleAvatar(
-                                  child: Image(
-                                image: AssetImage('img/akun.png'),
-                              )),
-                              trailing: Icon(Icons.more_vert),
-                              isThreeLine: true,
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (context, index) {
+                          // Firebase.initializeApp();
+                          DocumentSnapshot documentSnapshot =
+                              snapshot.data.documents[index];
+                          return SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Card(
+                              child: ListTile(
+                                title: Text(documentSnapshot.data()['name']),
+                                subtitle:
+                                    Text(documentSnapshot.data()['studentId']),
+                                leading: CircleAvatar(
+                                    child: Image(
+                                  image: AssetImage('img/akun.png'),
+                                )),
+                                trailing: Icon(Icons.more_vert),
+                                isThreeLine: true,
+                                onTap: () => navigateToDetail(context,
+                                    snapshot.data.documents[index]),
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     );
                   }
                 },
--- lib/HalamanDetailAspirasi.dart
+++ lib/HalamanDetailAspirasi.dart
@@ -1,60 +1,81 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blue,
-        accentColor: Colors.cyan),
-    home: DetailAspirasi(),
-  ));
+//   await Firebase.initializeApp();
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     theme: ThemeData(
+//         brightness: Brightness.light,
+//         primaryColor: Colors.blue,
+//         accentColor: Colors.cyan),
+//     home: DetailAspirasi(),
+//   ));
+// }
+
+// class DetailAspirasi extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: new Center(
+//         child: new Column(
+//           children: <Widget>[
+//             SingleChildScrollView(
+//               scrollDirection: Axis.vertical,
+//               child: StreamBuilder(
+//                 stream: Firestore.instance.collection("MyStudents").snapshots(),
+//                 builder: (context, snapshot) {
+//                   if (snapshot.hasData) {
+//                     return ListView.builder(
+//                       shrinkWrap: true,
+//                       itemCount: snapshot.data.documents.length,
+//                       itemBuilder: (context, index) {
+//                         // Firebase.initializeApp();
+//                         DocumentSnapshot documentSnapshot =
+//                             snapshot.data.documents[index];
+//                         return Card(
+//                           child: ListTile(
+//                             title: Text(documentSnapshot.data()['name']),
+//                             subtitle:
+//                                 Text(documentSnapshot.data()['studentId']),
+//                             leading: CircleAvatar(
+//                               child: Image(image: AssetImage('img/akun.png'),)
+//                             ),
+                            
+//                           ),
+//                         );
+//                       },
+//                     );
+//                   }
+//                 },
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+class DetailAspirasi extends StatefulWidget {
+
+  final DocumentSnapshot MyStudent;
+  DetailAspirasi({this.MyStudent});
+  @override
+  _DetailAspirasiState createState() => _DetailAspirasiState();
 }
 
-class DetailAspirasi extends StatelessWidget {
+class _DetailAspirasiState extends State<DetailAspirasi> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: new Center(
-        child: new Column(
-          children: <Widget>[
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: StreamBuilder(
-                stream: Firestore.instance.collection("MyStudents").snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, index) {
-                        // Firebase.initializeApp();
-                        DocumentSnapshot documentSnapshot =
-                            snapshot.data.documents[index];
-                        return Card(
-                          child: ListTile(
-                            title: Text(documentSnapshot.data()['name']),
-                            subtitle:
-                                Text(documentSnapshot.data()['studentId']),
-                            leading: CircleAvatar(
-                              child: Image(image: AssetImage('img/akun.png'),)
-                            ),
-                            
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
-            )
-          ],
-        ),
+      child: Card(
+        child: ListTile(
+          title: Text(widget.MyStudent.data()["name"])
+        )
       ),
     );
   }
